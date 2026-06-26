ff547d38aa5ff56dc18f3343bbfaca795a7aaf9e
Ntttho <nguyentatho14106@gmail.com>
2026-06-26 08:29:57 +0700

    first commit

---
+++ src/main/java/com/reptithcm/edu/BadmintonCourtManagerApplication.java
@@ -0,0 +1,13 @@
+package com.reptithcm.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonCourtManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadmintonCourtManagerApplication.class, args);
+    }
+
+}
